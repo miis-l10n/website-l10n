f5cf352164c8ec74a87abc83ae2c0e2d5a97eca3
nsuitter <nsuitter@saleforce.com>
2015-01-24 13:49:37 -0800

    Improving create_homework_folders script

--- setup/create_homework_folders.sh
+++ setup/create_homework_folders.sh
@@ -2,7 +2,7 @@
 
 
 ls -d students/* | while read student; do
-	grep "\/" Homework.md | grep -v "Final Project" | sed -e "s:Mon\. ::" -e "s:|.*| ::" -e "s:/:-:g" -e s":\(.*\) .*:homework-due-\1:" | while read hw; do
+	grep "\/" Homework.md | grep -v "Final Project" | sed -e "s/.* | Mon. \([^ ]*\).*/homework-due-\1/" -e "s/\//-/g" | while read hw; do
 		dir=$student/$hw
 		mkdir -p $dir
 		touch $dir/.keep
