a825beccfe6eab1a71d2f75f097da51ef66e7639
nsuitter <nsuitter@saleforce.com>
2015-01-24 12:49:36 -0800

    Adding .gitignore, .gitattributes, folder script, and students.txt

---
+++ make-dirs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+FILE='students.txt'
+while read line;
+do
+	fn=`echo $line | grep -v "#"`
+	# echo $fn
+	if [[ ! -d students ]]; then
+		mkdir students
+	fi
+	if [[ $fn != '' && ! -d students/$fn ]]; then
+		mkdir students/$fn
+		touch students/$fn/.keep
+	fi
+done < $FILE
