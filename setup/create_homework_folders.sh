a5c1baa0158633a3af88da8f04fc2e33b99ef9ba
nsuitter <nsuitter@saleforce.com>
2015-01-25 12:10:43 -0800

    Adding color function

--- setup/create_homework_folders.sh
+++ setup/create_homework_folders.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
+source setup/colorize.sh
 
 ls -d students/* | while read student; do
 	grep "\/" Homework.md | grep -v "Final Project" | sed -e "s/.* | Mon. \([^ ]*\).*/homework-due-\1/" -e "s/\//-/g" | while read hw; do
 		dir=$student/$hw
-		mkdir -p $dir
-		touch $dir/.keep
+		if [[ -d $dir ]]; then
+			string='Already existing'
+		else
+			string='Creating'
+			mkdir -p $dir
+			touch $dir/.keep
+		fi
+		echo $(green $string) $dir
 	done
 done
