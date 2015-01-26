6a14fdd13c7e899942c0c11f59d3661671e2dc6d
nsuitter <nsuitter@saleforce.com>
2015-01-26 13:50:21 -0800

    Coloring make-dirs output

--- setup/make-dirs.sh
+++ setup/make-dirs.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+source setup/colorize.sh
 FILE='groups.sh'
 while read line;
 do
 	fn=`echo $line | grep -v "#"`
 	# echo $fn
 	if [[ ! -d students ]]; then
-		mkdir students
+		dir='students'
+		mkdir $dir
+		echo $(green Creating:) $dir
 	fi
 	if [[ $fn != '' && ! -d students/$fn ]]; then
-		mkdir students/$fn
+		subdir="students/$fn"
+		mkdir $subdir
 		touch students/$fn/.keep
+		echo $(green Creating:) $subdir
 	fi
 done < $FILE
