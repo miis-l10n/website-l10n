03a64834a6c58f321fb00145b52dbe26460a1828
nsuitter <nsuitter@salesforce.com>
2015-02-02 15:32:37 -0800

    Create Homework.sh

---
+++ 02_shell_scripting/Homework.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Write a program that prints 99 bottles of beer on the wall.
+# The song starts with 
+# 99 bottles of beer on the wall
+# 99 bottles of beer!
+# You take one down and pass it around,
+# 98 bottles of beer on the wall!
+#
+# And ends with 
+# 1 bottle of beer on the wall
+# 1 bottle of beer!
+# You take one down and pass it around,
+# No more bottles of beer on the wall :-(
