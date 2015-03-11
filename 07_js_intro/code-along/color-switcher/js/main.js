9dfa7c31fa192d2e8c698724ab20108a10735e28
nsuitter <nsuitter@saleforce.com>
2015-03-10 18:21:35 -0700

    Updating code-along with method 1 included

--- 07_js_intro/code-along/color-switcher/js/main.js
+++ 07_js_intro/code-along/color-switcher/js/main.js
@@ -1,9 +1,35 @@
+// Method 1.
+// var body = document.querySelector("body")
+// var green = document.querySelector(".green")
+// var salmon = document.querySelector(".salmon")
+// var purple = document.querySelector(".purple")
+
+// green.addEventListener("click", turnBodyGreen)
+// salmon.addEventListener("click", turnBodySalmon)
+// purple.addEventListener("click", turnBodyPurple)
+
+// function turnBodyGreen() {
+//   body.setAttribute("class", "green")
+// }
+
+// function turnBodySalmon() {
+//   body.setAttribute("class", "salmon")
+// }
+
+// function turnBodyPurple() {
+//   body.setAttribute("class", "purple")
+// }
+
+// Method 2. Using the event object
+
+
 var list = document.querySelector("ul")
 var body = document.querySelector("body")
 
 list.addEventListener("click", changeColor)
 
 function changeColor(event) {
-  var color = event.target.getAttribute("class")
+  var elem = event.target
+  var color = elem.getAttribute("class")
   body.setAttribute("class", color)
 }
